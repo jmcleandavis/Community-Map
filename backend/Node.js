8e9e527c14e382d851c748bf9e00594775aec13a
Asher Green <76627281+ashergreen82@users.noreply.github.com>
2024-06-29 16:55:39 -0400

    Copy the community sale map html file and modified it to work with Axios, added Node.js file in the backend.

---
+++ backend/Node.js
@@ -0,0 +1,23 @@
+// backend/index.js
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../garage-sale-map-client/build')));
+
+// API endpoint to serve addresses
+app.get('/api/addresses', (req, res) => {
+    const addresses = require('./addresses.json');
+    res.json(addresses);
+});
+
+// Catch-all to serve the React app for any route not handled
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../garage-sale-map-client/build/index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
