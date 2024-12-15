e7b44cc33da1b40e57f0bfb21730b5a306a0a6cb
Asher Green <76627281+ashergreen82@users.noreply.github.com>
2024-12-14 19:23:33 -0500

    Manageed to get it working with google maps.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
 function App() {
-  const [count, setCount] = useState(0)
-  console.log('Rendering App component');
   const [position, setPosition] = useState(null);
+  const mapContainerStyle = {
+    width: '100%',
+    height: '100vh'
+  };
+
+  const defaultCenter = {
+    lat: 51.505,
+    lng: -0.09
+  };
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
       (position) => {
-        setPosition([position.coords.latitude, position.coords.longitude]);
+        setPosition({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        });
       },
       () => {
         console.log('Geolocation error');
@@ -23,19 +30,20 @@ function App() {
 
   return (
     <div>
-      <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "100vh", width: "100%" }}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution="&copy; <a href='https://osm.org/copyright'>OpenStreetMap</a> contributors"
-        />
-        {position && (
-          <Marker position={position}>
-            <Popup>
-              You are here!
-            </Popup>
-          </Marker>
-        )}
-      </MapContainer>
+      <LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+        <GoogleMap
+          mapContainerStyle={mapContainerStyle}
+          center={position || defaultCenter}
+          zoom={13}
+        >
+          {position && (
+            <Marker
+              position={position}
+              title="You are here!"
+            />
+          )}
+        </GoogleMap>
+      </LoadScript>
     </div>
   )
 }
