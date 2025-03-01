0c0926ec18d2342ea98e66483b74270b59f74324
Asher Green <76627281+ashergreen82@users.noreply.github.com>
2025-03-01 15:56:57 -0500

    Got rid of the V7 start transition warnings.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, createRoutesFromChildren, matchRoutes } from 'react-router-dom'
 import App from './App.jsx'
 import { GarageSalesProvider } from './context/GarageSalesContext'
 import { SearchProvider } from './context/SearchContext'
 import { SelectionProvider } from './context/SelectionContext'
 import './index.css'
 
+// Configure future flags for React Router v7 compatibility
+const router = {
+  future: {
+    v7_startTransition: true,
+    v7_relativeSplatPath: true
+  }
+};
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <GarageSalesProvider>
     <SearchProvider>
       <SelectionProvider>
-        <BrowserRouter>
+        <BrowserRouter {...router}>
           <App />
         </BrowserRouter>
       </SelectionProvider>
