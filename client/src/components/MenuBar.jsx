4d23ad88191b6f1d7ec34ed94eac59727ce1ea0e
Asher Green <76627281+ashergreen82@users.noreply.github.com>
2025-03-18 19:18:55 -0400

    Removed the Map View item from the menu bar.

--- client/src/components/MenuBar.jsx
+++ client/src/components/MenuBar.jsx
@@ -13,7 +13,6 @@ const MenuBar = () => {
     <nav className="menu-bar">
       <div className="menu-bar-item" onClick={() => handleNavigation('/about')}>About</div>
       <div className="menu-bar-item" onClick={() => handleNavigation('/info')}>Info</div>
-      <div className="menu-bar-item" onClick={() => handleNavigation('/')}>Map View</div>
       <div className="menu-bar-item" onClick={() => handleNavigation('/admin/community-sales')}>Manage Community Sales</div>
       <div className="menu-bar-item" onClick={() => handleNavigation('/login')}>Login</div>
     </nav>
